4d52df87e22adf025f21c73b95739831d3139cc0
Nguyen Huy <hyquaq@gmail.com>
2022-11-27 23:06:40 +0700

    push notification on android > 8.0

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.musicplayer;
 
+import static android.content.ContentValues.TAG;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
 		implements View.OnClickListener {
+	
+	private static final int NOTIFICATION_ID = 1508;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +35,32 @@ public class MainActivity extends AppCompatActivity
 	public void onClick(View view) {
 		if (view.getId() == R.id.button) {
 			sendNotification();
+			Log.d(TAG,
+			      "onClick: ok");
 		}
 	}
 	
 	private void sendNotification() {
+		Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+		                                             R.drawable.ic_launcher_background);
+		Notification notification = new NotificationCompat.Builder(this,
+		                                                           MyApplication.CHANNEL_ID)
+				.setContentTitle("Title push notification")
+				.setContentText("Content here")
+				.setSmallIcon(R.drawable.ic_notification_custom)
+				.setLargeIcon(bitmap)
+				.build();
+		
+		NotificationManager notificationManager
+				= (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		
+		notificationManager.notify(getNotificationId(),
+		                           notification);
+		Log.d(TAG,
+		      "sendNotification: notify");
+	}
 	
+	private int getNotificationId() {
+		return (int) new Date().getTime();
 	}
 }
